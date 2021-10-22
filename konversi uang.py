e395aed227b6f61c9663e430b171f83a342b56fd
AexonJP <92908255+AexonJP@users.noreply.github.com>
2021-10-22 16:09:33 +0800

    Update konversi uang.py

--- konversi uang.py
+++ konversi uang.py
@@ -6,52 +6,73 @@ hi = ("NOMINAL UANG ")
 ke = (" ANDA: ")
 dalam = ("NOMINAL UANG ANDA SETELAH DI KONVERSIKAN ADALAH ")
 nama = (" RUPIAH"," DOLLAR AMERIKA"," DOLLAR SINGAPURA"," EURO"," YEN")
+salah = ("FORMAT YANG ANDA MASUKKAN SALAH")
 
 while (True):
     print ("KONVERSI MATA UANG\n","1. IDR\n","2. USD\n","3. SGD\n","4. EUR\n","5. JPY\n")
     awal = input ("APA MATA UANG AWAL ANDA? ")
     akhir = input ("MAU DI KONVERSIKAN KEMANA? ")
+
+
     print ("\n")
 
     if awal == "1":
         frt = input (f"{hi}\b{nama[0]}{ke}")
-        break
+        if frt < "a" :
+            print ()
+        else :
+            akhir = 0
     elif awal == "2":
         kop = input (f"{hi}\b{nama[1]}{ke}")
-        frt = float(kop)*usd
-        break
+        if kop < "a" :
+            frt = float(kop)*usd
+        else :
+            akhir = 0
     elif awal == "3":
         kop = input (f"{hi}\b{nama[2]}{ke}")
-        frt = float(kop)*sgd
-        break
+        if kop <"a" :
+            frt = float(kop)*sgd
+        else :
+            akhir = 0
     elif awal == "4":
         kop = input (f"{hi}\b{nama[3]}{ke}")
-        frt = float(kop)*eur
-        break
+        if kop <"a" :
+             frt = float(kop)*eur
+        else :
+            akhir = 0
     elif awal == "5":
         kop = input (f"{hi}\b{nama[4]}{ke}")
-        frt = float(kop)*jpy
+        if kop <"a" :
+            frt = float(kop)*jpy
+        else :
+            akhir = 0
+    else :
+        akhir = 0
+
+    if akhir == "1":
+        nilai = float(frt) / 1
+        kl = nama[0]
+        break
+    if akhir == "2":
+        nilai = float(frt)/float(usd)
+        kl = nama[1]
+        break
+    if akhir == "3":
+        nilai = float(frt)/float(sgd)
+        kl = nama[2]
+        break
+    if akhir == "4":
+        nilai = float(frt)/float(eur)
+        kl = nama[3]
+        break
+    if akhir == "5":
+        nilai = float(frt)/float(jpy)
+        kl = nama [4]
         break
     else :
-        print ("FORMAT YANG ANDA MASUKKAN SALAH")
-
-if akhir == "1":
-    nilai = float(frt) / 1
-    kl = nama[0]
-if akhir == "2":
-    nilai = float(frt)/float(usd)
-    kl = nama[1]
-if akhir == "3":
-    nilai = float(frt)/float(sgd)
-    kl = nama[2]
-if akhir == "4":
-    nilai = float(frt)/float(eur)
-    kl = nama[3]
-if akhir == "5":
-    nilai = float(frt)/float(jpy)
-    kl = nama [4]
+        print (f"\n{salah}\n")
 ex = round(nilai, 2)
 ribuan = f"{ex:,}"
 print (f"{dalam}{ribuan}{kl}")
 if ex == 0.0:
-    print("\nitu duit atau harga diriku dimatamu sih? kok ga ada harganya banget")
+    print("\nitu duit atau harga diriku dimatamu sih? kok ga ada harganya banget\n")
